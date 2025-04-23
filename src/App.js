30828f4431f70f01f49a8a131a07c793a24e0427
tanglin <tanglinespanol@outlook.com>
2025-04-23 11:04:20 +0800

    修复一个小问题

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router";
-import { useQuery } from '@tanstack/react-query'
-
-export default function App(){
+//import { useQuery } from '@tanstack/react-query'
+const PUBLICPATH = PUBLIC_PATH || '/';
+const Home = function(){
     return (
-        <BrowserRouter basename='/'>
+        <div>
+            <h1>Home</h1>
+        </div>
+    )
+}
+export default function App(){
+
+    return (
+        <BrowserRouter basename={PUBLICPATH}>
             <Routes>
-                <Route path="/" element={<App />} />
+                <Route path="/" element={<Home />} />
             </Routes>
         </BrowserRouter>
     )
